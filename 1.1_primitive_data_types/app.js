6315c002a1d7c2255e1cc9cc5a3f824baf956ec1
ReaGol <reutgoldin@gmail.com>
2022-09-18 14:25:37 +0300

    ex1

---
+++ 1.1_primitive_data_types/app.js
@@ -0,0 +1,18 @@
+let str = 'a'
+let num = 1
+let obj = {
+    name : 'Reut'
+}
+let bool = false
+let nothing = Null
+let empty = undefined
+let smbl = Symbol('foo')
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof obj)
+console.log(typeof bool)
+console.log(typeof nothing)
+console.log(typeof empty)
+console.log(typeof smbl)
+
