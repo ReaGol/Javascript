a50416f3a56c5f2ed82c987162fdafe212ac206e
ReaGol <reutgoldin@gmail.com>
2022-09-18 18:12:00 +0300

    ex4

---
+++ 2.2_functions/index.js
@@ -0,0 +1,16 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
+  let str =
+    "You will be a " +
+    jobTitle +
+    " in " +
+    location +
+    " married to " +
+    partnerName +
+    " with " +
+    numberOfChildren +
+    " children";
+  return str;
+}
+
+tellFortune();
+console.log(str);
