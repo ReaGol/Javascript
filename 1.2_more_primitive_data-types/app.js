8c3bde6c6d5b6cd033c8971498aa78c5acb63276
ReaGol <reutgoldin@gmail.com>
2022-09-18 15:44:08 +0300

    ex2

---
+++ 1.2_more_primitive_data-types/app.js
@@ -0,0 +1,56 @@
+// 1. Which of the following is/are strings?
+// a) 4
+// b) 4.0
+c) '4'
+// 2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+// c) '4'
+d) -4
+// 3. Which of the following is/are booleans?
+a) true
+b) false
+// c) "true"
+// 4. What is the result of 80 + 71.2?
+a) 151.2
+// b) 151
+// c) 8071.2
+// 5. What is the result of "80" + 71.2?
+// a) 151.2
+// b) 151
+c) "8071.2"
+// 6. Does 100 + 30 produce a number or a string?
+a) number
+// b)string
+// 7. Does "100" + 30 produce a number or a string?
+// a)number
+b)string
+// */
+//------Submit your answers under the
+// question------//
+//create three different ways to declare variables
+var myName = 'Reut';
+let myAge = 39;
+const myGender = 'female'
+//declare a variable and reassign a value to it
+let myName = 'Reut';
+myName = 'Moshe'
+//create a variable and after that assign a string
+// with its value being: He's got it!
+var sentence = "He's got it!"
+
+// 1. create a variable and assign a value on how
+// much a restaurant bill is.
+let bill = 200;
+// 2. create a variable and assign a value on how
+// much tax they should pay.
+let tax = 0.17;
+// 3. create a variable that will calculate the bill
+// * tax and its output would be: Your total bill is
+/* <total bill> $. */
+let totalBill = `your total bill is ${bill+(bill*tax)}`;
+
+// Round the number 50.6 to its nearest integer
+Math.round(50.6)
+//Create a variable that is undefined
+var x
