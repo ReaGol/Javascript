6a54d9526eca2d127b39dd21432b8414aaa7c64f
ReaGol <reutgoldin@gmail.com>
2022-10-11 08:40:13 +0300

    ex

---
+++ 11.2_forEachMapping/index.js
@@ -0,0 +1,71 @@
+// // 1)
+// let integerArray = [1, 2, 3, 4, 5, 6, 7, 8];
+let stringArray = ["a", "b", "c", "d", "e"];
+// function doubleValues(array) {
+//   return integerArray.map((element) => element * 2);
+// }
+
+// // 2)
+// function onlyEvenValues(arr2) {
+//   evenArray = [];
+//   arr2.forEach((element) => {
+//     if (element % 2 == 0) {
+//       evenArray.push(element);
+//     }
+//   });
+//   return evenArray;
+// }
+
+// onlyEvenValues(integerArray);
+
+// // 3)
+
+// function showFirstAndLast(arr3) {
+//   itemsArray = [];
+//   arr3.forEach((element, index) => {
+//     if (index === 0 || index === arr3.length - 1) {
+//       itemsArray.push(element.toString);
+//     }
+//   });
+//   return itemsArray;
+// }
+
+// showFirstAndLast(stringArray);
+
+// // 4)
+// let string1 = "the quick brown fox";
+
+// function vowelCount(str1) {
+//   stringArray = str1.split("");
+//   const vowelCountNewObj = {};
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   stringArray.forEach((letter) => {
+//     if (vowels.includes(letter) && !vowelCountNewObj[letter]) {
+//       // vowelCountNewObj[letter] = letter;
+//       vowelCountNewObj[letter] = 1;
+//       console.log(vowelCountNewObj);
+//     } else if (vowels.includes(letter)) {
+//       vowelCountNewObj[letter]++;
+//     console.log(vowelCountNewObj);
+//     }
+//   });
+//   return vowelCountNewObj;
+// }
+// console.log(vowelCount(string1));
+
+
+// // // 5)
+// function capitalize(str) {
+//   return str.toUpperCase();
+// }
+// console.log(capitalize('strong'))
+
+// // // 6)
+// // function shiftLetters(str) {
+// // // foreach
+// // }
+//  bcde - abcd
+// // // 7)
+// // function swapCase(str){
+// // //  map
+// // }
---
+++ 12.3_school_method/index.js
@@ -0,0 +1,52 @@
+const school = {
+  teachers: [
+    {
+      id: 1,
+      name: "Pinchas",
+      subjects: ["chemistry", "biology", "physics"],
+      students: [],
+      capacityLeft: 3,
+    },
+    {
+      id: 2,
+      name: "Williams",
+      subjects: ["history", "ethics"],
+      students: [],
+      capacityLeft: 2,
+    },
+  ],
+  students: [
+    {
+      id: 10,
+      name: "Jennifer",
+      age: 20,
+    },
+    {
+      id: 11,
+      name: "Howard",
+      age: 23,
+    },
+    {
+      id: 12,
+      name: "Old-Timmy",
+      age: 86,
+    },
+    {
+      id: 13,
+      name: "Houston",
+      age: 21,
+    },
+  ],
+};
+
+// method findPerson
+school.assignTeachersSubject = function(id, subject) {
+ const teacher = school.teachers.find(teacher => teacher.id === id)
+  if (teacher) {
+    if (teacher.subjects.includes(subject)){
+        teacher.subjects.push(subject);
+    }
+    
+  }
+}
+
