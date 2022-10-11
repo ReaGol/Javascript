44f7d25b3f4e0a7762aac430073bbf7996e6054f
ReaGol <reutgoldin@gmail.com>
2022-10-11 08:56:02 +0300

    ex

--- 12.3_school_method/index.js
+++ 12.3_school_method/index.js
@@ -40,13 +40,39 @@ const school = {
 };
 
 // method findPerson
+school.findPerson = function (type, id) {
+  const students = this[type];
+  const person = students.find((e, i) => {
+    return e.id === id;
+  });
+  return person;
+};
+
+school.assignStudent = function (id, subject) {
+  const student = school.findPerson('students', id);
+  if (!student) {
+    return 'No student with the specified id was found';
+  }
+  const teacher = school.teachers.find((e) => {
+    return e.subjects.includes(subject);
+  });
+
+  if (teacher.capacityLeft < 1) {
+    return 'Sorry, no available teachers left';
+  } else {
+    teacher.students.push(student);
+    teacher.capacityLeft--;
+    return teacher;
+  }
+};
 school.assignTeachersSubject = function(id, subject) {
  const teacher = school.teachers.find(teacher => teacher.id === id)
   if (teacher) {
     if (teacher.subjects.includes(subject)){
-        teacher.subjects.push(subject);
+        teacher.subjects.push(subject.toLowerCase());
+        return teacher;
     }
-    
   }
 }
 
+console.log(school.assignTeachersSubject(2, 'Math'));
